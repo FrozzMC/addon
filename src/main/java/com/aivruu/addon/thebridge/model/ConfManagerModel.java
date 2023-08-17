bf6ef9ee8b671a85b490d136fdcecba076658629
InitSync <benjaalejandro206@gmail.com>
2023-08-17 14:06:49 -0400

    chore(conf-manager-model): change method name from `load` to `wasLoaded`.

--- src/main/java/com/aivruu/addon/thebridge/model/ConfManagerModel.java
+++ src/main/java/com/aivruu/addon/thebridge/model/ConfManagerModel.java
@@ -4,7 +4,18 @@ import com.aivruu.addon.thebridge.model.config.ConfModel;
 import org.jetbrains.annotations.NotNull;
 
 public interface ConfManagerModel {
-	boolean load();
+	/**
+	 * Returns a boolean value if the configuration were loaded correctly.
+	 *
+	 * @return True if the configuration were loaded, else return false.
+	 */
+	boolean wasLoaded();
 	
+	/**
+	 * Returns the {@code ConfModel} object for the configuration. If the object is null (configuration not loaded)<p>
+	 * will throw an {@code IllegalStateException} exception.
+	 *
+	 * @return A {@code ConfModel} object.
+	 */
 	@NotNull ConfModel config();
 }
