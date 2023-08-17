020bf08fbb4757b9f342d00ad3cdf507adeab0df
InitSync <benjaalejandro206@gmail.com>
2023-08-17 14:08:02 -0400

    chore: rename listener to `GameScoreboardListener`

---
+++ src/main/java/com/aivruu/addon/thebridge/listener/GameScoreboardListener.java
@@ -0,0 +1,42 @@
+package com.aivruu.addon.thebridge.listener;
+
+import com.aivruu.addon.thebridge.model.ScoreboardManagerModel;
+import com.google.common.base.Preconditions;
+import eu.mip.alandioda.bridge.spigot.event.GameStateChangeEvent;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class GameScoreboardListener implements Listener {
+	private final ScoreboardManagerModel scoreboardManager;
+	
+	public GameScoreboardListener(final @NotNull ScoreboardManagerModel scoreboardManager) {
+		this.scoreboardManager = Preconditions.checkNotNull(scoreboardManager, "ScoreboardManagerModel object cannot be null.");
+	}
+	
+	@EventHandler (priority = EventPriority.LOW)
+	void onGameJoin(final PlayerCommandPreprocessEvent event) {
+		if (event.isCancelled()) return;
+		
+		Player player = event.getPlayer();
+		final String id = player.getUniqueId().toString();
+		
+		if (event.getMessage().contains("join") && scoreboardManager.findOrNull(id) == null) {
+			// Removes old scoreboard.
+			player.setScoreboard(null);
+			// Creates new scoreboard to player.
+			scoreboardManager.create(id);
+			return;
+		}
+		
+		player = null;
+	}
+	
+	@EventHandler (priority = EventPriority.LOW)
+	void onStateChange(final GameStateChangeEvent event) {
+		scoreboardManager.update(event.getGame());
+	}
+}
