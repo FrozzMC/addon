639b2cce8a2691ac3c4aca9066a85c1cc89d864b
InitSync <benjaalejandro206@gmail.com>
2023-08-18 18:27:56 -0400

    chore(main): remove unused imports, include mavenCentral repository before download libs, add log messages and remove `loadConfigurationComponents()` method

--- src/main/java/com/aivruu/addon/thebridge/ScoreboardAddonPlugin.java
+++ src/main/java/com/aivruu/addon/thebridge/ScoreboardAddonPlugin.java
@@ -6,19 +6,11 @@ import com.aivruu.addon.thebridge.impl.SimpleConfManagerModelImpl;
 import com.aivruu.addon.thebridge.listener.GameScoreboardListener;
 import com.aivruu.addon.thebridge.model.ConfManagerModel;
 import com.aivruu.addon.thebridge.model.ScoreboardManagerModel;
-import com.aivruu.addon.thebridge.model.config.ConfModel;
 import com.aivruu.addon.thebridge.utils.LoggerUtils;
 import com.google.common.base.Preconditions;
 import eu.mip.alandioda.bridge.spigot.TheBridge;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-import space.arim.dazzleconf.ConfigurationFactory;
-import space.arim.dazzleconf.ConfigurationOptions;
-import space.arim.dazzleconf.ext.snakeyaml.CommentMode;
-import space.arim.dazzleconf.ext.snakeyaml.SnakeYamlConfigurationFactory;
-import space.arim.dazzleconf.ext.snakeyaml.SnakeYamlOptions;
-import space.arim.dazzleconf.helper.ConfigurationHelper;
-import space.arim.dazzleconf.sorter.AnnotationBasedSorter;
 
 public final class ScoreboardAddonPlugin extends JavaPlugin {
 	private static ScoreboardAddonPlugin plugin;
@@ -43,18 +35,22 @@ public final class ScoreboardAddonPlugin extends JavaPlugin {
 	public void onLoad() {
 		plugin = this;
 		
+		// Add the maven central repo for the dependencies.
+		LoggerUtils.info("Adding maven central repo for download dependencies...");
+		LibraryHandler.addMavenCentral();
+		
 		// Load the required libraries by the plugin.
-		LibraryHandler.addCustomRepo("https://nexus.iridiumdevelopment.net/repositories/maven-releases/");
+		LoggerUtils.info("Downloading required libraries for the plugin.");
 		LibraryHandler.loadLibraries(
+			"com{}github{}VelexNetwork:iridium-color-api:1.2.0",
 			"fr{}mrmicky:fastboard:2.0.0",
-			"org{}yaml:snakeyaml:1.33",
 			"space{}arim{}dazzleconf:dazzleconf-core:1.3.0-M2",
-			"space{}arim{}dazzleconf:dazzleconf-ext-snakeyaml:1.3.0-M2",
-			"com{}iridium:IridiumColorAPI:1.0.6"
+			"space{}arim{}dazzleconf:dazzleconf-ext-snakeyaml:1.3.0-M2"
 		);
 		
+		LoggerUtils.info("Loading configuration components...");
 		// Load the components for the configuration before load the files.
-		loadConfigurationComponents();
+		confManager = new SimpleConfManagerModelImpl(getDataFolder().toPath());
 		if (!confManager.wasLoaded()) {
 			LoggerUtils.error("Could not load configuration correctly.");
 			setEnabled(false);
@@ -66,15 +62,24 @@ public final class ScoreboardAddonPlugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
+		LoggerUtils.info("Registering plugin listener...");
 		getServer().getPluginManager()
 			.registerEvents(new GameScoreboardListener(JavaPlugin.getPlugin(TheBridge.class), scoreboardManager), plugin);
 		
+		LoggerUtils.info("Registering plugin command...");
 		Preconditions.checkNotNull(getCommand("thebridgeaddon")).setExecutor(new MainCommand(confManager, scoreboardManager));
+	
+		LoggerUtils.info(
+			"The plugin has been enabled successful!",
+			String.format("Running on version %s", Constants.VERSION)
+		);
 	}
 	
 	@Override
 	public void onDisable() {
+		LoggerUtils.info("The plugin is being disabled...");
 		if (scoreboardManager != null) {
+			LoggerUtils.info("Clearing memory used by the plugin...");
 			scoreboardManager.clear();
 			scoreboardManager = null;
 		}
@@ -86,25 +91,5 @@ public final class ScoreboardAddonPlugin extends JavaPlugin {
 		if (plugin != null) {
 			plugin = null;
 		}
-	}
-	
-	private void loadConfigurationComponents() {
-		// Set default options for the YAML file.
-		final SnakeYamlOptions yamlOptions = new SnakeYamlOptions.Builder()
-			.commentMode(CommentMode.fullComments())
-			.build();
-		// Set default options for the configuration.
-		final ConfigurationOptions buildOptions = new ConfigurationOptions.Builder()
-			.sorter(new AnnotationBasedSorter())
-			.build();
-		// Creates a ConfigurationFactory object for the configuration model (ConfModel).
-		final ConfigurationFactory<ConfModel> confFactory = SnakeYamlConfigurationFactory.create(
-			ConfModel.class,
-			buildOptions,
-			yamlOptions
-		);
-		
-		// Initializes ConfManager using the parameters given.
-		confManager = new SimpleConfManagerModelImpl(new ConfigurationHelper<>(getDataFolder().toPath(), "config.yml", confFactory));
 	}
 }
