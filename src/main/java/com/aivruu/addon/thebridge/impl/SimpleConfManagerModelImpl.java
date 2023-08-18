ed110a736a1c63ff7cacef259bff164b5113df7a
InitSync <benjaalejandro206@gmail.com>
2023-08-18 18:35:20 -0400

    chore(config-manager-impl): change parameter type to a `Path` reference and create new `ConfigurationHelper` reference in the same constructor of the class

--- src/main/java/com/aivruu/addon/thebridge/impl/SimpleConfManagerModelImpl.java
+++ src/main/java/com/aivruu/addon/thebridge/impl/SimpleConfManagerModelImpl.java
@@ -2,21 +2,33 @@ package com.aivruu.addon.thebridge.impl;
 
 import com.aivruu.addon.thebridge.model.ConfManagerModel;
 import com.aivruu.addon.thebridge.model.config.ConfModel;
-import com.aivruu.addon.thebridge.utils.LoggerUtils;
-import com.google.common.base.Preconditions;
 import org.jetbrains.annotations.NotNull;
 import space.arim.dazzleconf.error.InvalidConfigException;
 import space.arim.dazzleconf.helper.ConfigurationHelper;
+import space.arim.dazzleconf.ConfigurationOptions;
+import space.arim.dazzleconf.ext.snakeyaml.CommentMode;
+import space.arim.dazzleconf.ext.snakeyaml.SnakeYamlConfigurationFactory;
+import space.arim.dazzleconf.ext.snakeyaml.SnakeYamlOptions;
+import space.arim.dazzleconf.sorter.AnnotationBasedSorter;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class SimpleConfManagerModelImpl implements ConfManagerModel {
 	private final ConfigurationHelper<ConfModel> confHelper;
 	
 	private ConfModel config;
 	
-	public SimpleConfManagerModelImpl(final @NotNull ConfigurationHelper<ConfModel> confHelper) {
-		this.confHelper = Preconditions.checkNotNull(confHelper, "ConfigurationHelper object for ConfModel configuration model cannot be null.");
+	public SimpleConfManagerModelImpl(final @NotNull Path dataFolder) {
+		confHelper = new ConfigurationHelper<>(dataFolder, "config.yml", SnakeYamlConfigurationFactory.create(
+			ConfModel.class,
+			new ConfigurationOptions.Builder()
+			.sorter(new AnnotationBasedSorter())
+			.build(),
+			new SnakeYamlOptions.Builder()
+			.commentMode(CommentMode.alternativeWriter())
+			.build()
+		));
 	}
 	
 	@Override
