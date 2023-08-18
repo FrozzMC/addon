bc8d096ec6a2735a11c9e14e4896dcd0b4a3c304
InitSync <benjaalejandro206@gmail.com>
2023-08-18 18:21:51 -0400

    chore(command): use `PlaceholderUtils` class to process colors instead of `IridiumColorAPI` class

--- src/main/java/com/aivruu/addon/thebridge/cmds/MainCommand.java
+++ src/main/java/com/aivruu/addon/thebridge/cmds/MainCommand.java
@@ -3,8 +3,8 @@ package com.aivruu.addon.thebridge.cmds;
 import com.aivruu.addon.thebridge.Constants;
 import com.aivruu.addon.thebridge.model.ConfManagerModel;
 import com.aivruu.addon.thebridge.model.ScoreboardManagerModel;
+import com.aivruu.addon.thebridge.utils.PlaceholderUtils;
 import com.google.common.base.Preconditions;
-import com.iridium.iridiumcolorapi.IridiumColorAPI;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -35,11 +35,11 @@ public class MainCommand implements CommandExecutor {
 		final Player player = (Player) sender;
 		
 		if (args.length == 0) {
-			player.sendMessage(IridiumColorAPI.process(String.format(
+			player.sendMessage(PlaceholderUtils.colorize(String.format(
 				"&a&l| TheBridgeAddon &ais running on &a&lBukkit-%s",
 				Bukkit.getBukkitVersion()
 			)));
-			player.sendMessage(IridiumColorAPI.process(String.format(
+			player.sendMessage(PlaceholderUtils.colorize(String.format(
 				"&a&l| &aAddon current version: &e%s",
 				Constants.VERSION
 			)));
@@ -48,36 +48,36 @@ public class MainCommand implements CommandExecutor {
 		
 		switch (args[0]) {
 			default:
-				player.sendMessage(IridiumColorAPI.process("&c&l| &cUnknown sub-command specified."));
+				player.sendMessage(PlaceholderUtils.colorize("&c&l| &cUnknown sub-command specified."));
 				break;
 			case "help":
-				IridiumColorAPI.process(Arrays.asList(
+				PlaceholderUtils.colorize(Arrays.asList(
 					"&a&lTheBridgeAddon &8| &aAdmin Help Commands",
 					"&7 - &b/tba help &fShows this message.",
 					"&7 - &b/tba reload &fReloads the plugin."
 				)).forEach(player::sendMessage);
 				break;
 			case "reload":
-				player.sendMessage(IridiumColorAPI.process("&c&l| &cBy do this could happen an error during this process."));
-				player.sendMessage(IridiumColorAPI.process("&c&l| &cWe recommend restart the server for this."));
+				player.sendMessage(PlaceholderUtils.colorize("&c&l| &cBy do this could happen an error during this process."));
+				player.sendMessage(PlaceholderUtils.colorize("&c&l| &cWe recommend restart the server for this."));
 				
 				if (args.length == 1) {
-					player.sendMessage(IridiumColorAPI.process("&c&l| &cIf you want to reload the plugin, type &e/sba reload confirm"));
+					player.sendMessage(PlaceholderUtils.colorize("&c&l| &cIf you want to reload the plugin, type &e/sba reload confirm"));
 					break;
 				}
 				
 				if (args[1].equals("confirm")) {
 					if (!confManager.wasLoaded()) {
-						player.sendMessage(IridiumColorAPI.process("&c&l| &cConfiguration could not be reloaded correctly."));
+						player.sendMessage(PlaceholderUtils.colorize("&c&l| &cConfiguration could not be reloaded correctly."));
 						break;
 					}
 					
 					scoreboardManager.reload();
-					player.sendMessage(IridiumColorAPI.process("&a&l| &aPlugin reloaded correctly!"));
+					player.sendMessage(PlaceholderUtils.colorize("&a&l| &aPlugin reloaded correctly!"));
 					break;
 				}
 				
-				player.sendMessage(IridiumColorAPI.process("&c&l| &cReload confirmation cancelled."));
+				player.sendMessage(PlaceholderUtils.colorize("&c&l| &cReload confirmation cancelled."));
 		}
 		
 		return false;
