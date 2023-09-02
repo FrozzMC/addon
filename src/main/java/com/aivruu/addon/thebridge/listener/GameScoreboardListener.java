5a3eb0198e8a1d8d8308375a68341091bbe12607
InitSync <benjaalejandro206@gmail.com>
2023-09-02 00:04:33 -0400

    chore(GameScoreboardListener): simplify code structure

--- src/main/java/com/aivruu/addon/thebridge/listener/GameScoreboardListener.java
+++ src/main/java/com/aivruu/addon/thebridge/listener/GameScoreboardListener.java
@@ -2,9 +2,6 @@ package com.aivruu.addon.thebridge.listener;
 
 import com.aivruu.addon.thebridge.model.ScoreboardManagerModel;
 import com.google.common.base.Preconditions;
-import eu.mip.alandioda.bridge.spigot.TheBridge;
-import eu.mip.alandioda.bridge.spigot.event.GameStateChangeEvent;
-import eu.mip.alandioda.bridge.spigot.game.Game;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,37 +10,25 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class GameScoreboardListener implements Listener {
-	private final TheBridge bridgePlugin;
 	private final ScoreboardManagerModel scoreboardManager;
 	
-	public GameScoreboardListener(final @NotNull TheBridge bridgePlugin, final @NotNull ScoreboardManagerModel scoreboardManager) {
-		this.bridgePlugin = Preconditions.checkNotNull(bridgePlugin, "TheBridge reference cannot be null.");
+	public GameScoreboardListener(final @NotNull ScoreboardManagerModel scoreboardManager) {
 		this.scoreboardManager = Preconditions.checkNotNull(scoreboardManager, "ScoreboardManagerModel object cannot be null.");
 	}
 	
 	@EventHandler (priority = EventPriority.LOW)
 	void onGameJoin(final PlayerCommandPreprocessEvent event) {
-		if (event.isCancelled()) return;
-		
+		// Checks if the event was cancelled.
+		if (event.isCancelled()) {
+			return;
+		}
 		final Player player = event.getPlayer();
-		final String id = player.getUniqueId().toString();
-
 		// The command executed contains the 'join' argument?
 		// Also, check if the player have a scoreboard created (by this plugin).
-		if (event.getMessage().contains("join") && scoreboardManager.findOrNull(id) == null) {
-			// Removes old scoreboard.
-			player.setScoreboard(null);
-			
-			final Game game = bridgePlugin.GetGameByPlayer(player);
-			// If the reference is null, the player isn't playing.
-			if (game == null) return;
-			
-			scoreboardManager.create(player, game);
+		if (!event.getMessage().contains("join") || scoreboardManager.findOrNull(player.getUniqueId().toString()) != null) {
+			return;
 		}
-	}
-	
-	@EventHandler (priority = EventPriority.LOW)
-	void onStateChange(final GameStateChangeEvent event) {
-		scoreboardManager.update(event.getGame());
+		player.setScoreboard(null);
+		scoreboardManager.create(player);
 	}
 }
