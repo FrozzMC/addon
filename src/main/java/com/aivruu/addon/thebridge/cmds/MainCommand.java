3e7b26b3a1e8d04514ec83f9b60302ba35780235
InitSync <benjaalejandro206@gmail.com>
2023-09-02 00:08:51 -0400

    chore(MainCommand): improve command method structure

--- src/main/java/com/aivruu/addon/thebridge/cmds/MainCommand.java
+++ src/main/java/com/aivruu/addon/thebridge/cmds/MainCommand.java
@@ -1,6 +1,7 @@
 package com.aivruu.addon.thebridge.cmds;
 
 import com.aivruu.addon.thebridge.Constants;
+import com.aivruu.addon.thebridge.enums.Result;
 import com.aivruu.addon.thebridge.model.ConfManagerModel;
 import com.aivruu.addon.thebridge.model.ScoreboardManagerModel;
 import com.aivruu.addon.thebridge.utils.PlaceholderUtils;
@@ -30,16 +31,16 @@ public class MainCommand implements CommandExecutor {
 		final @NotNull String label,
 		final @NotNull String[] args
 	) {
-		if (!(sender instanceof Player)) return false;
-		
+		if (!(sender instanceof Player)) {
+			return false;
+		}
 		final Player player = (Player) sender;
-		
 		// The player has permission for this?
 		if (!player.hasPermission(Constants.HANDLE_COMMAND_PERM)) {
 			player.sendMessage(PlaceholderUtils.colorize("&c&l| &cYou don't have permission for this!"));
 			return false;
 		}
-		
+		// Checks if the command have arguments.
 		if (args.length == 0) {
 			PlaceholderUtils.colorize(Arrays.asList(
 				"&a&lTheBridgeAddon &8| &aAdmin Help Commands",
@@ -48,27 +49,27 @@ public class MainCommand implements CommandExecutor {
 			)).forEach(player::sendMessage);
 			return false;
 		}
-		
-		switch (args[0]) {
-			default: return false;
-			case "about":
-				player.sendMessage(PlaceholderUtils.colorize("&a&l| TheBridgeAddon &ais running on Bukkit - &a&l" + Bukkit.getBukkitVersion()));
-				player.sendMessage(PlaceholderUtils.colorize("&a&l| &aAddon current version: &e" + Constants.VERSION));
-				break;
-			case "reload":
-				sender.sendMessage(PlaceholderUtils.colorize("&c&l| &cBy do this could happen an error during this process. We recommend restart the server for this."));
-				
-				// The configuration was reloaded correctly?
-				if (!confManager.wasLoaded()) {
-					sender.sendMessage(PlaceholderUtils.colorize("&c&l| &cConfiguration could not be loaded correctly."));
-					return false;
-				}
-				
-				scoreboardManager.reload();
-				sender.sendMessage(PlaceholderUtils.colorize("&a&l| &aPlugin reloaded correctly!"));
-				break;
+		// Checks if the first argument is 'about'.
+		// Else, check if the argument is 'reload'.
+		if (args[0].equalsIgnoreCase("about")) {
+			player.sendMessage(PlaceholderUtils.colorize(String.format("&a&l| TheBridgeAddon &ais running on Bukkit - &a&l%s", Bukkit.getBukkitVersion())));
+			player.sendMessage(PlaceholderUtils.colorize(String.format("&a&l| &aAddon current version: &e%s", Constants.VERSION)));
+			return false;
+		} else if (args[0].equalsIgnoreCase("reload")) {
+			sender.sendMessage(PlaceholderUtils.colorize("&c&l| &cBy do this could happen an error during this process. We recommend restart the server for this."));
+			// The configuration was reloaded correctly?
+			if (confManager.load() == Result.NO_CONFIG_LOAD) {
+				sender.sendMessage(PlaceholderUtils.colorize("&c&l| &cConfiguration could not be loaded correctly."));
+				return false;
+			}
+			for (final Player online : Bukkit.getOnlinePlayers()) {
+				scoreboardManager.remove(online.getUniqueId().toString());
+				scoreboardManager.create(online);
+			}
+			sender.sendMessage(PlaceholderUtils.colorize("&a&l| &aScoreboards and Configuraton has been reloaded correctly!"));
+			return false;
 		}
-		
+		player.sendMessage(PlaceholderUtils.colorize("&c&l| Unknown sub-command specified!"));
 		return false;
 	}
 }
