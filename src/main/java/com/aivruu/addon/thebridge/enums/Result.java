1e7402dd0035d75d7ddf68b6fbc3bb8cc5b61981
InitSync <benjaalejandro206@gmail.com>
2023-09-01 23:57:48 -0400

    feat(Result): implement enum for handle internal operations

---
+++ src/main/java/com/aivruu/addon/thebridge/enums/Result.java
@@ -0,0 +1,16 @@
+package com.aivruu.addon.thebridge.enums;
+
+public enum Result {
+	/**
+	 * Indicates that the operation was completed successfully.
+	 */
+	SUCCESS,
+	/**
+	 * Indicates that there was an error during the config load process.
+	 */
+	NO_CONFIG_LOAD,
+	/**
+	 * Indicates that the scoreboard cannot be deleted correcty.
+	 */
+	NO_SCOREBOARD_REMOVE
+}
