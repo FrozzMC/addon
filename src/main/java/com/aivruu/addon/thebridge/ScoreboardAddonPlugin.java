20e8f9d51a80603bd57b4d9be5fdb4027d4ac092
InitSync <benjaalejandro206@gmail.com>
2023-09-02 00:12:19 -0400

    chore(ScoreboardAddonPlugin): remove get() method, modify config load check in onLoad() method, call to the ScoreboardManagerModel.start() method in onEnable(), and use ScoreboardManagerModel.stop() method in onDisable()

--- src/main/java/com/aivruu/addon/thebridge/ScoreboardAddonPlugin.java
+++ src/main/java/com/aivruu/addon/thebridge/ScoreboardAddonPlugin.java
@@ -2,6 +2,7 @@ package com.aivruu.addon.thebridge;
 
 import com.aivruu.addon.thebridge.cmds.MainCommand;
 import com.aivruu.addon.thebridge.cmds.completer.MainCommandTabCompleter;
+import com.aivruu.addon.thebridge.enums.Result;
 import com.aivruu.addon.thebridge.impl.ScoreboardManagerModelImpl;
 import com.aivruu.addon.thebridge.impl.SimpleConfManagerModelImpl;
 import com.aivruu.addon.thebridge.listener.GameScoreboardListener;
@@ -9,34 +10,15 @@ import com.aivruu.addon.thebridge.model.ConfManagerModel;
 import com.aivruu.addon.thebridge.model.ScoreboardManagerModel;
 import com.aivruu.addon.thebridge.utils.LoggerUtils;
 import com.google.common.base.Preconditions;
-import eu.mip.alandioda.bridge.spigot.TheBridge;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 public final class ScoreboardAddonPlugin extends JavaPlugin {
-	private static ScoreboardAddonPlugin plugin;
-	
-	private ConfManagerModel confManager;
-	private ScoreboardManagerModel scoreboardManager;
-	
-	/**
-	 * Returns the current {@code ScoreboardAddonPlugin} instance.
-	 *
-	 * @return A {@code ScoreboardAddonPlugin} type instance.
-	 */
-	public static @NotNull ScoreboardAddonPlugin get() {
-		if (plugin == null) {
-			throw new IllegalStateException("Could not get ScoreboardAddonPlugin instance because is null.");
-		}
-		
-		return plugin;
-	}
+	private ConfManagerModel confManagerModel;
+	private ScoreboardManagerModel scoreboardManagerModel;
 	
 	@Override
 	public void onLoad() {
-		plugin = this;
-		
 		// Add the repositories for the dependencies.
 		LoggerUtils.info("Adding repositories for download dependencies...");
 		LibraryHandler.addMavenCentral();
@@ -47,33 +29,32 @@ public final class ScoreboardAddonPlugin extends JavaPlugin {
 		LibraryHandler.loadLibraries(
 			"com{}github{}VelexNetwork:iridium-color-api:1.2.0",
 			"fr{}mrmicky:fastboard:2.0.0",
-			"space{}arim{}dazzleconf:dazzleconf-core:1.3.0-M2",
-			"space{}arim{}dazzleconf:dazzleconf-ext-snakeyaml:1.3.0-M2"
+			String.format("space{}arim{}dazzleconf:dazzleconf-core:%s", Constants.CONFIGURATION_LIBRARY_RELEASE),
+			String.format("space{}arim{}dazzleconf:dazzleconf-ext-snakeyaml:%s", Constants.CONFIGURATION_LIBRARY_RELEASE)
 		);
-		
 		LoggerUtils.info("Loading configuration components...");
 		// Load the components for the configuration before load the files.
-		confManager = new SimpleConfManagerModelImpl(getDataFolder().toPath());
-		if (!confManager.wasLoaded()) {
+		confManagerModel = new SimpleConfManagerModelImpl(getDataFolder().toPath());
+		if (confManagerModel.load() == Result.NO_CONFIG_LOAD) {
 			LoggerUtils.error("Could not load configuration correctly.");
 			setEnabled(false);
 			return;
 		}
 		
-		scoreboardManager = new ScoreboardManagerModelImpl(plugin, confManager.config(), getServer().getScheduler());
+		scoreboardManagerModel = new ScoreboardManagerModelImpl(this, confManagerModel.config());
 	}
 	
 	@Override
 	public void onEnable() {
+		scoreboardManagerModel.start();
+		
 		LoggerUtils.info("Registering plugin listener...");
-		getServer().getPluginManager().registerEvents(
-			new GameScoreboardListener(JavaPlugin.getPlugin(TheBridge.class), scoreboardManager),
-			plugin
-		);
+		getServer().getPluginManager()
+			.registerEvents(new GameScoreboardListener(scoreboardManagerModel), this);
 		
 		LoggerUtils.info("Registering plugin command...");
 		final PluginCommand command = Preconditions.checkNotNull(getCommand("thebridgeaddon"));
-		command.setExecutor(new MainCommand(confManager, scoreboardManager));
+		command.setExecutor(new MainCommand(confManagerModel, scoreboardManagerModel));
 		command.setTabCompleter(new MainCommandTabCompleter());
 		
 		LoggerUtils.info(
@@ -87,10 +68,9 @@ public final class ScoreboardAddonPlugin extends JavaPlugin {
 		LoggerUtils.info("The plugin is being disabled...");
 
 		// The ScoreboardManagerModel reference not been deleted yet?
-		if (scoreboardManager != null) {
-			LoggerUtils.info("Clearing memory used by the plugin...");
-			scoreboardManager.clear();
-			scoreboardManager = null;
+		if (scoreboardManagerModel != null) {
+			LoggerUtils.info("Stopping all plugin running tasks.");
+			scoreboardManagerModel.stop();
 		}
 	}
 }
