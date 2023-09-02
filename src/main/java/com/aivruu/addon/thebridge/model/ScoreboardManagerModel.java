c217361235276468cb517355d8efc2e9b0c81ee3
InitSync <benjaalejandro206@gmail.com>
2023-09-02 00:13:54 -0400

    chore(ScoreboardManagerModel): remove reload() and clear() methods, make to remove() method return a Result enum type, and implement new start() and boards() methods

--- src/main/java/com/aivruu/addon/thebridge/model/ScoreboardManagerModel.java
+++ src/main/java/com/aivruu/addon/thebridge/model/ScoreboardManagerModel.java
@@ -1,50 +1,62 @@
 package com.aivruu.addon.thebridge.model;
 
+import com.aivruu.addon.thebridge.enums.Result;
 import eu.mip.alandioda.bridge.spigot.game.Game;
 import fr.mrmicky.fastboard.FastBoard;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
+
 public interface ScoreboardManagerModel {
 	/**
-	 * Tries to return the {@code FastBoard} object for that id. If not exist will return a null value.
+	 * Tries to return the {@link  FastBoard} object for that id. If not exist will return a null value.
 	 *
-	 * @param id {@code UUID} as {@code String} of player.
+	 * @param id Unique player ID as {@link String}.
 	 *
-	 * @return The {@code FastBoard} object or null.
+	 * @return The {@link FastBoard} object or null.
 	 */
 	@Nullable FastBoard findOrNull(final @NotNull String id);
 	
 	/**
-	 * Creates a new FastBoard object (Scoreboard) to player.
+	 * Returns a {@link Collection} object with all the {@link FastBoard} registered references (scoreboards).
 	 *
-	 * @param player {@code Player} object for that player.
-	 * @param game {@code Game} object for the game where is the player.
+	 * @return A {@link Collection} reference.
 	 */
-	void create(final @NotNull Player player, final @NotNull Game game);
+	@NotNull Collection<FastBoard> boards();
 	
 	/**
-	 * Removes the scoreboard to player.
-	 *
-	 * @param id {@code UUID} as {@code String} of player.
+	 * Starts the required plugin tasks for the future scoreboards.
 	 */
-	void remove(final @NotNull String id);
+	void start();
 	
 	/**
+	 * Stores the scoreboard for this new player.
+	 *
+	 * @param player player scoreboard holder.
+	 */
+	void create(final @NotNull Player player);
+	
+	/**
+	 * Tries to remove the scoreboard to player and return a {@link Result} enum type.
+	 *
+	 * @param id Unique player ID as {@link String}.
+	 *
+	 * @return A {@link Result} enum type.
+	 */
+ 	@NotNull Result remove(final @NotNull String id);
+	
+ 	/**
 	 * Updates the content for that scoreboard.
 	 *
-	 * @param game {@code Game} object for that arena.
+	 * @param board {@link FastBoard} object for the update.
+	 * @param game {@link Game} object for that arena.
 	 */
-	void update(final @NotNull Game game);
+	void update(final @NotNull FastBoard board, final @NotNull Game game);
 	
-	/**
-	 * Reload all the scoreboards in cache to forceupdate their contents.
+ 	/**
+	 * Cancels the current plugin tasks and remove all the scoreboards from memory cache.
 	 */
-	void reload();
-	
-	/**
-	 * Removes all the objects in cache.
-	 */
-	void clear();
+	void stop();
 }
