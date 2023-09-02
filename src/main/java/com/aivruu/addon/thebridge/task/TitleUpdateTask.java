9157e977504b52a8e086ac4e135b6b179e60001b
InitSync <benjaalejandro206@gmail.com>
2023-09-01 23:59:59 -0400

    chore(TitleUpdateTask): simplify structure and use byte primitive instead of int type for task current index and text list size

--- src/main/java/com/aivruu/addon/thebridge/task/TitleUpdateTask.java
+++ src/main/java/com/aivruu/addon/thebridge/task/TitleUpdateTask.java
@@ -1,5 +1,6 @@
 package com.aivruu.addon.thebridge.task;
 
+import com.aivruu.addon.thebridge.model.ScoreboardManagerModel;
 import com.aivruu.addon.thebridge.utils.PlaceholderUtils;
 import com.google.common.base.Preconditions;
 import fr.mrmicky.fastboard.FastBoard;
@@ -9,33 +10,31 @@ import org.jetbrains.annotations.NotNull;
 import java.util.List;
 
 public class TitleUpdateTask extends BukkitRunnable {
-	private final FastBoard board;
+	private final ScoreboardManagerModel scoreboardManagerModel;
 	private final List<String> content;
-	private final int titleSize;
 	
-	private int rate;
+	private byte index;
 	
-	public TitleUpdateTask(final @NotNull FastBoard board, final @NotNull List<String> content, final int rate) {
-		this.board = Preconditions.checkNotNull(board, "CachedBoardModel object cannot be null.");
+	public TitleUpdateTask(final @NotNull ScoreboardManagerModel scoreboardManagerModel, final @NotNull List<String> content, final byte index) {
+		this.scoreboardManagerModel = Preconditions.checkNotNull(scoreboardManagerModel, "ScoreboardManagerModel reference cannot be null.");
 		this.content = Preconditions.checkNotNull(content, "Text list cannot be null.");
-		this.rate = rate;
-		titleSize = content.size();
+		this.index = index;
 	}
 	
 	@Override
 	public void run() {
+		final byte size = (byte) content.size();
 		// Checks if the update-rate is higher or equal than the collection size.
-		if (rate >= (titleSize - 1)) {
-			rate = 0;
+		if (index >= (size - 1)) {
+			index = 0;
 		}
-		
-		rate++;
-		
-		// The board has been deleted?
-		if (!board.isDeleted()) {
-			board.updateTitle(PlaceholderUtils.parse(board.getPlayer(), content.get(rate)));
-		} else {
-			cancel();
+		index++;
+		for (final FastBoard board : scoreboardManagerModel.boards()) {
+			// Checks if the player has disabled their scoreboard.
+			if (board.isDeleted()) {
+				continue;
+			}
+			board.updateTitle(PlaceholderUtils.parse(board.getPlayer(), content.get(index)));
 		}
 	}
 }
