a21e969dd5b472782acffbc822bbd46fca5531dd
Qekly <benjaalejandro206@gmail.com>
2023-09-04 00:08:21 -0300

    chore: modify field name

--- src/main/java/com/aivruu/addon/thebridge/ScoreboardAddonPlugin.java
+++ src/main/java/com/aivruu/addon/thebridge/ScoreboardAddonPlugin.java
@@ -29,7 +29,7 @@ public final class ScoreboardAddonPlugin extends JavaPlugin {
 		LibraryHandler.loadLibraries(
 			"com{}github{}VelexNetwork:iridium-color-api:1.2.0",
 			"fr{}mrmicky:fastboard:2.0.0",
-			String.format("org{}spongepowered:configurate-gson:%s", Constants.CONFIGURATION_LIBRARY_RELEASE)
+			String.format("org{}spongepowered:configurate-gson:%s", Constants.CONFIGURATE_VERSION)
 		);
 		LoggerUtils.info("Loading configuration components...");
 		// Load the components for the configuration before load the files.
